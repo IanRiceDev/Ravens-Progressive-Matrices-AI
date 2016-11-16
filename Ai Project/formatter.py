13688719bfd0903c766620e079da83f2529808ad
IanRiceDev <the3bears@centurylink.net>
2016-11-16 12:46:58 -0500

    progress!

--- Ai Project/formatter.py
+++ Ai Project/formatter.py
@@ -1,24 +1,31 @@
 
 read = open ('ai.txt','r')
-#read1 = open ('ai.txt','r')
+read1 = open ('ai.txt','r')
 first = read.readline()
 first =  read.readline()
-#two = read1.readline()
-test = first
-dec1 = []
-#dec2 = []
-print test.split("\n")
-test.split("\n")
-#test.
-#test1 = test.pop[]
+two = read1.readline()
 
-dec1.append(test.split("\n"))
-print dec1
-dec1.pop(1)
-#dec2.append(two)
+dec1 = []
+dec2 = []
+#print test.split("\n")
+first.split("\n")
+#test.
+#test1 = dec1.li
+
+dec1.insert(0,first.split("\n"))
+
+
+dec2.append(two)
 #print test1
 
+
+
+
+
+del dec1[0][1]
+
+print dec1
 #print dec2
 #print(cmp(dec2,dec1))
 read.close()
-#read1.close()
+read1.close()
