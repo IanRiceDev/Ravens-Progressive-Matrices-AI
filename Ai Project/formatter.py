fc2e126a470e6c7d03b476a223166fc5f9eb7019
IanRiceDev <the3bears@centurylink.net>
2016-11-29 09:01:11 -0500

    update

--- Ai Project/formatter.py
+++ Ai Project/formatter.py
@@ -1,7 +1,8 @@
-
 read = open ('ai.txt','r')
 read1 = open ('ai.txt','r')
+
 readcount1 = 0
+
 while (readcount1 < 2):
     first = read.readline()
     readcount1 += 1
@@ -10,37 +11,45 @@ while (readcount1 < 2):
 
 two = read1.readline()
 
-dec = []
-dec2 = []
-#print test.split("\n")
-#two.split()
-#test.
-#test1 = dec1.li
-#dec1.insert(0,two.split(" "))
-
-dec.insert(0,two.split(".",))
-print dec.__str__()
-print dec.__len__()
-dec.insert(1,two.split("\n"))
-dec.index("cube")
-
-
-
- #test.split("\n")
-
-#dec1[0].split("\n")
-
-#dec2.insert(0,first.split("\n"))
-#print dec1
+dec = [('a', two.split(".")),('b', 1),('c', 2),('d', 3),('e', 4)]
 
 
 
 
 
-#del dec1[0][2]
-#del dec2[0][1]
-print dec
-#print dec2
-#print(cmp(dec1,dec2))
+
+dec1 = dict(dec)
+print dec1#
+print dec1['a']#
+list1 = list(dec1['a'])
+print list1#
+
+
+
+
+
+
+
+
+
+list2 = str(list1[1]).split('\n')
+print list2#
+list1.insert(1,list2[0])
+print list1#
+
+list1Lang = list1.__len__()
+print list1Lang#
+
+del list1[list1Lang - 1]
+print list1#
+
+
+
+
+
+
+
+
+
 read.close()
 read1.close()
