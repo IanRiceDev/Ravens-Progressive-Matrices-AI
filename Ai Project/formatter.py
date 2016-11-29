40984240f22d34ab001e3ed31af83c470d422dd0
IanRiceDev <the3bears@centurylink.net>
2016-11-29 09:52:29 -0500

    update

--- Ai Project/formatter.py
+++ Ai Project/formatter.py
@@ -21,7 +21,7 @@ dec = [('1', two.split(".")),('b', 1),('c', 2),('d', 3),('e', 4)]
 dec1 = dict(dec)
 print dec1#
 print dec1['1']#
-list1 = list(dec1['1'])
+list1 = dec1['1']
 print list1#
 
 
@@ -42,7 +42,13 @@ print list1Lang#
 
 del list1[list1Lang - 1]
 print list1#
-dec1.update(dict(list1))
+
+
+
+dec1.update({'1' : list1[0]})
+dec1.update({'c' : list1[1]})
+print cmp('1','c')
+print dec1
 
 
 
