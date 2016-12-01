bf71e98c36fb1b38792a99c06615b2c132703a3c
IanRiceDev <the3bears@centurylink.net>
2016-12-01 09:55:21 -0500

    auto update

--- Ai Project/.idea/test.py
+++ Ai Project/.idea/test.py
@@ -1,6 +1,9 @@
-dec1 = {"2":233,"3":50}
-list = ('a', 0),('b', 1),('c', 2),('d', 3),('e', 4)
-dec2 = {"2":54}
-dec2.update(dec1)
-listdic = dict(list)
-print(listdic)
+#dec1 = {"2":233,"3":50}
+#list = ('a', 0),('b', 1),('c', 2),('d', 3),('e', 4)
+#dec2 = {"2":54}
+#dec2.update(dec1)
+#listdic = dict(list)
+#print(listdic)
+list3 = [3,4,5,"find"]
+
+list
--- Ai Project/formatter.py
+++ Ai Project/formatter.py
@@ -11,16 +11,17 @@ while (readcount1 < 2):
 
 two = read1.readline()
 
-dec = [(1, two.split("."))]
+dec = [(0, two.split("."))]
 
 
 
 
 
-
+print dec
 dec1 = dict(dec)
 print dec1#
-list1 = dec1[1]
+list2Lang = dec1.__len__()
+list1 = dec1[0]
 print list1#
 
 
@@ -29,24 +30,33 @@ print list1#
 
 
 
-
-
-list2 = str(list1[1]).split('\n')
-print list2#
-list1.insert(1,list2[0])
-print list1#
 
 list1Lang = list1.__len__()
-print list1Lang#
 
-del list1[list1Lang - 1]
+list2 = str(list1[list1Lang - 1]).split('\n')
+print list2#
+
+list1.insert(list1Lang - 1,list2[0])
+
+
+
+print list1Lang#
+#list1[list1Lang - 1].split("\n")
+print list1
+del list1[list1Lang]
 print list1#
 
-
+x = 0
 dec1.clear()
-dec1.update({1 : list1[0]})
-dec1.update({2 : list1[1]})
-print cmp(dec1[1],dec1[2])#
+dec1 = {x:0}
+while  x > list1Lang:
+
+    dec1.update({x: list1[x]})
+    x += x
+    if  x > list1Lang:
+        break
+
+#print cmp(dec1[1],dec1[2])#
 print dec1#
 
 
