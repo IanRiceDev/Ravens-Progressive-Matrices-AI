709ff0b02ffc60112903f06a4ef09be9f691ad20
IanRiceDev <the3bears@centurylink.net>
2016-12-15 12:47:16 -0500

    update

--- ai/formatter.py
+++ ai/formatter.py
@@ -1,22 +1,22 @@
 read = open ('problem1.txt','r')
 read1 = open ('problem1.txt','r')
-# opens text file were problem dicrepson is stored
+# opens text file were problem description is stored
 readcount1 = 0
-#declaers counter var for loop
+#declares counter Variable for loop
 first = read.readline()
-#declaers var and puts text from frist line in string
+#declares Variable and puts text from frist line in string
 while (readcount1 < 1):
 
     first = read.readline()
-    # enters loop and redeclaers the var to the text on the second line
+    #enters loop and redeclares the var to the text on the second line
     readcount1 += 1
     #increses the value of the counter and then exits the loop
 
 
 two = read1.readline()
-#declears a var and puts info from line one
+#declares a Variable and puts info from line one
 dec = [(0, two.split("."))]
-#declears a array and puts the value of 0 at index 0 and adds the value of line one and splits and delets all "."
+#declares a array and puts the value of 0 at index 0 and adds the value of line one and splits and deletes all periods
 
 
 
@@ -25,9 +25,9 @@ dec1 = dict(dec)
 #convters dec1 to dictonary
 print dec1#
 list2Lang = dec1.__len__()
-#declars var eqwile to dec1's langthe
+#declares Variable equal to dec1's length
 list1 = dec1[0]
-#declars var eqwile to dec1 at 0 index
+#declares Variable equal to dec1 at 0 index
 print list1#
 
 
@@ -38,7 +38,7 @@ print list1#
 
 
 list1Lang = list1.__len__()
-#declaers var eqwil to the lang of list1
+#declares Variable equal to the length of list1
 list2 = str(list1[list1Lang - 1]).split('\n')
 
 print list2#
@@ -50,7 +50,7 @@ list1.insert(list1Lang - 1,list2[0])
 print list1Lang#
 print list1
 del list1[list1Lang]
-#delets list at value of it's lang
+#delets list at value of it's length
 print list1#
 
 
@@ -68,4 +68,4 @@ dec1.clear()
 
 read.close()
 read1.close()
-#closes connectsons to text file
+#closes connections to text file
