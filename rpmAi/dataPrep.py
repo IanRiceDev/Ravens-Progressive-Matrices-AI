1a01bb4b28a94b6559f3a65a263364a67a269ea4
IanRiceDev <the3bears@centurylink.net>
2017-10-11 11:53:43 -0400

    update

--- rpmAi/dataPrep.py
+++ rpmAi/dataPrep.py
@@ -1,2 +1,12 @@
-import numpy
-import pandas
+from xml.dom import minidom
+
+xmldoc = minidom.parse('rpmai.xml')
+
+itemlist = xmldoc.getElementsByTagName('item')
+
+print(len(itemlist))
+
+print(itemlist[0].attributes['name'].value)
+
+for s in itemlist:
+    print(s.attributes['name'].value)
